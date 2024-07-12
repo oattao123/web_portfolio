ef939bad9e53aa513e8b1587ddd013c86bdc6d2c
oattao123 <dceriythrrmkic@gmail.com>
2024-07-12 11:58:15 +0700

    delect contact

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -23,10 +23,10 @@ const links = [
         name: 'work',
         path: '/work'
     },
-    {
-        name: 'contact',
-        path: '/contact'
-    }
+    // {
+    //     name: 'contact',
+    //     path: '/contact'
+    // }
 ]
 
 const MobileNav = () => {
