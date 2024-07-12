b3d2c7f6215c5892ddbc66f6b49b8d70239057e1
oattao123 <dceriythrrmkic@gmail.com>
2024-07-12 11:50:57 +0700

    exit page home

--- app/page.jsx
+++ app/page.jsx
@@ -8,14 +8,14 @@ import Stats from "@/components/Stats";
 const Home = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
-      <div className="container mx-auto h-full ">
+      <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left order-2 xl:order-none">
-          <span className="text-xl font-medium mb-2">Developer</span>
-          <h1 className="h1 mb-6">
-            Hello I'm <br /> <span className="text-amber-500">Dollatham Charoenthammakit</span>
-          </h1>
-          <p className="max-w-[500px] mb-9 text-white/80 mx-auto xl:mx-0">
+          <div className="text-center xl:text-left order-2 xl:order-none mb-8 xl:mb-0">
+            <span className="text-xl font-medium mb-2">Developer</span>
+            <h1 className="text-3xl xl:text-5xl mb-6">
+              Hello I'm <br /> <span className="text-amber-500">Dollatham Charoenthammakit</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80 mx-auto xl:mx-0">
               I innovate to create solutions that help everyone lead a better, more fulfilling life. By leveraging technology and creative problem-solving, I aim to enhance well-being and make a positive impact on the world.
             </p>
 
@@ -25,7 +25,7 @@ const Home = () => {
                 size="lg" 
                 className="uppercase flex items-center gap-2"
               >
-                <span className="text-amber-500">Download CV</span>
+                <span className="text-amber-500 text-base xl:text-lg">Download CV</span>
                 <FiDownload className="h-6 w-6" />
               </Button>
               <div className="mt-8 xl:mb-0">
@@ -37,13 +37,14 @@ const Home = () => {
                 />
               </div>
             </div>
+          </div>
 
-        </div>        
-        <div className="order-1 xl:order-none mb-8 xl:mb-0">
-          <Photo />
+          <div className="order-1 xl:order-none">
+            <Photo />
+          </div>
         </div>
       </div>
-      </div>
+      
       <Stats/>
     </section>
   );
